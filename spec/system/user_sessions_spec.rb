a518a68dffa4246149f233b771af2122770788b7
epcocryo <epcsgokb@gmail.com>
2026-09-13 04:32:29 +0900

    ログイン用のデバッグコードの追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe 'UserSessions', type: :system do
 
         click_button 'ログイン'
 
+        puts "===== LOGIN DEBUG ====="
+        puts "URL: #{page.current_url}"
+        puts page.html
+
         expect(page).to have_current_path(root_path)
         expect(page).to have_link('ログアウト', visible: false)
       end
