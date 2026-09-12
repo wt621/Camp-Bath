1888b15b7ff8ecd3d8a9b0bb64c0ee7446c68d7c
epcocryo <epcsgokb@gmail.com>
2026-09-13 01:55:46 +0900

    test:ログアウトのbeforeにログイン成功確認を追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -54,8 +54,9 @@ RSpec.describe 'UserSessions', type: :system do
 
       fill_in 'Eメール', with: user.email
       fill_in 'パスワード', with: password
-
       click_button 'ログイン'
+      
+      expect(page).to have_current_path(root_path)
     end
 
     it 'ログアウト処理が成功する' do
